54ef3620271ed7a61039be17fccccdaf633ba820
andrengln <andreasnainggolan32@gmail.com>
2026-05-13 10:20:14 +0700

    soal2.c

--- soal2.c
+++ soal2.c
@@ -5,14 +5,14 @@ int dy[] = {0, -1, 0, 1, 1, 1, -1, -1};
 
 
 void dfs(char** grid, int r, int c, int n, int m) {
-    
-    if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] != 'L')
+    // Check bounds
+    if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] != '1')
         return;
 
-    
-    grid[r][c] = 'W';
+    // Mark cell as visited
+    grid[r][c] = '0';
 
-    
+    // Explore all 8 directions
     for (int i = 0; i < 8; i++) {
         int newR = r + dx[i];
         int newC = c + dy[i];
@@ -20,13 +20,12 @@ void dfs(char** grid, int r, int c, int n, int m) {
     }
 }
 
-
 int countIslands(char** grid, int n, int m) {
     int count = 0;
 
     for (int r = 0; r < n; r++) {
         for (int c = 0; c < m; c++) {
-            if (grid[r][c] == 'L') {
+            if (grid[r][c] == '1') {
                 count++;
                 dfs(grid, r, c, n, m);
             }
@@ -34,5 +33,21 @@ int countIslands(char** grid, int n, int m) {
     }
 
     return count;
+}
+
+
+int main(){
+    int M,N;
+    char grid;
+    
+    scanf("%d %d", &M, &N);
+
+
+    countIslands(grid,M,N);
+        printf("ISLANDS %d" , countIslands);
+        printf("LARGEST %d" );
+
+        return 0;
+
 
 }
