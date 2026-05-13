e647b4c8e9782d8e4347f63043f62f402ab3f088
andrengln <andreasnainggolan32@gmail.com>
2026-05-13 10:55:31 +0700

    soal2.c

--- soal2.c
+++ soal2.c
@@ -1,41 +1,42 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int dx[] = {-1, 0, 1, 0, 1, -1, -1, 1};
 int dy[] = {0, -1, 0, 1, 1, 1, -1, -1};
 
-
+// Perform DFS to mark all connected 'L' as visited
 void dfs(char** grid, int r, int c, int n, int m) {
-    
-    if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] != 'L')
-        return;
-
-    
-    grid[r][c] = 'W';
-
-    
-    for (int i = 0; i < 8; i++) {
-        int newR = r + dx[i];
-        int newC = c + dy[i];
-        dfs(grid, newR, newC, n, m);
-    }
+	// Check bounds
+	if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] != 'L')
+		return;
+	
+	// Mark cell as visited
+	grid[r][c] = 'W';
+	
+	// Explore all 8 directions
+	for (int i = 0; i < 8; i++) {
+		int newR = r + dx[i];
+		int newC = c + dy[i];
+		dfs(grid, newR, newC, n, m);
+	}
 }
 
+// Count the number of islands
 int countIslands(char** grid, int n, int m) {
-    int count = 0;
-
-    for (int r = 0; r < n; r++) {
-        for (int c = 0; c < m; c++) {
-            if (grid[r][c] == '1') {
-                count++;
-                dfs(grid, r, c, n, m);
-            }
-        }
-    }
-
-    return count;
+	int count = 0;
+	
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < m; c++) {
+			if (grid[r][c] == 'L') {
+				count++;
+				dfs(grid, r, c, n, m);
+			}
+		}
+	}
+	
+	return count;
 }
-
 
 int main(){
     int M,N;
