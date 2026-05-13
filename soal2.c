7cde6f796a100d81e2119fb5ffebf44dcdabfee8
andrengln <andreasnainggolan32@gmail.com>
2026-05-13 09:53:57 +0700

    soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int dx[] = {-1, 0, 1, 0, 1, -1, -1, 1};
+int dy[] = {0, -1, 0, 1, 1, 1, -1, -1};
+
+
+void dfs(char** grid, int r, int c, int n, int m) {
+    
+    if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] != 'L')
+        return;
+
+    
+    grid[r][c] = 'W';
+
+    
+    for (int i = 0; i < 8; i++) {
+        int newR = r + dx[i];
+        int newC = c + dy[i];
+        dfs(grid, newR, newC, n, m);
+    }
+}
+
+
+int countIslands(char** grid, int n, int m) {
+    int count = 0;
+
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < m; c++) {
+            if (grid[r][c] == 'L') {
+                count++;
+                dfs(grid, r, c, n, m);
+            }
+        }
+    }
+
+    return count;
+
 }
